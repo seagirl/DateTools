798ea89cf0badf20e7c69b004c570735e877ee3b
Takuho Yoshizu <161193+seagirl@users.noreply.github.com>
2023-01-16 13:38:53 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,8 +3,24 @@ import PackageDescription
 
 let package = Package(
     name: "DateToolsSwift",
+    platforms: [
+        .iOS(.v11),
+        .tvOS(.v11),
+        .watchOS(.v4)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "DateToolsSwift",
+            targets: ["DateToolsSwift"])
+    ],
     targets: [
-        Target(name: "DateToolsSwift")
-    ]
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "DateToolsSwift",
+            dependencies: [],
+            path: "DateToolsSwift/DateTools")
+    ],
+    swiftLanguageVersions: [.v5]
 )
-package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
